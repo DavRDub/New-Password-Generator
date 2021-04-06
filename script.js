88bc451e74e5fdfe9d75b0d6ce193815bc3cae3c
David Whitesel <whiteseldavid@gmail.com>
2021-04-06 09:11:21 -0700

    adding confirms

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
 var Special = ["#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]"];
 var Numbers = [0,1,2,3,4,5,6,7,8,9];
 
+var UPPERValid = confirm("Would you like your password to contain uppercase letters?");
+var lowerValid = confirm("Would you like your password to contain lowercase letters?");
+var SpecialValid = confirm("Would you like your password to contain special character symbols?");
+var NumbersValid = confirm("Would you like your password to contain numbers?");
+
+
 
 // Write password to the #password input
 function writePassword() {
