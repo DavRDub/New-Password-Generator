5fcb7c2ba46cfba255049bb5f582469c6eff128c
David Whitesel <whiteseldavid@gmail.com>
2021-04-06 10:26:52 -0700

    adding confirms

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
 
 var UPPER = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -10,6 +10,7 @@ var UPPERValid = confirm("Would you like your password to contain uppercase lett
 var lowerValid = confirm("Would you like your password to contain lowercase letters?");
 var SpecialValid = confirm("Would you like your password to contain special character symbols?");
 var NumbersValid = confirm("Would you like your password to contain numbers?");
+
 
 
 
